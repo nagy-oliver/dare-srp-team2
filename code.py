f819d6d13e85b5fcab49cc8ecb61915c210f42c2
nagy-oliver <oliver.nagy510@gmail.com>
2024-05-09 16:03:22 +0200

    sensor printouts

--- code.py
+++ code.py
@@ -11,6 +11,28 @@ print(f'Read in a delay of {delay_pyro_miliseconds} ms from the config file')
 
 states = statemachine.Statemachine(PYRO_FIRE_DELAY_MS = delay_pyro_miliseconds)
 
-while True:
-   buzzer.buzzer_tick()
-   states.tick()
+i2c = board.I2C()
+
+import adafruit_adxl34x
+accelerometer = adafruit_adxl34x.ADXL343(i2c)
+# accelerometer.acceleration
+
+import adafruit_bmp280
+bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+bmp280.sea_level_pressure = 1013.25
+# bmp280.pressure
+# bmp280.altitude
+
+with open("data.txt", "w") as fp:
+   while True:
+      buzzer.buzzer_tick()
+      states.tick()
+
+      fp.write(f"{accelerometer.acceleration} \n")
+      fp.write(f"Pressure: {bmp280.pressure}, Altitude: {bmp280.altitude} \n")
+      # print(accelerometer.acceleration)
+      # print(bmp280.pressure)
+      # print(bmp280.altitude)
+
+
+# external.PYRO_DETONATE()
