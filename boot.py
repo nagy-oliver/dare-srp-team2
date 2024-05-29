b734956336f836997637bd4021d958cb75efec5f
nagy-oliver <oliver.nagy510@gmail.com>
2024-05-29 10:17:27 +0200

    most recent

--- boot.py
+++ boot.py
@@ -1,12 +1,19 @@
 import board
 import storage
+import digitalio
+import external
 
 # Remame drive to 'SRP-DARE-FC' 
 storage.remount('/', readonly=False)
 
+switch = external._ext_led
+switch.direction = digitalio.Direction.INPUT
+switch.pull = digitalio.Pull.UP
+
+
 m = storage.getmount('/')
 m.label = 'SRP-DARE-FC'
 
-storage.remount('/', readonly=True)
+storage.remount('/', readonly=not switch.value)
 
 storage.enable_usb_drive()
--- code.py
+++ code.py
@@ -5,34 +5,60 @@ import measure
 import statemachine
 import buzzer
 import config
+import time
 
 delay_pyro_miliseconds = config.get_deployment_timer()
 print(f'Read in a delay of {delay_pyro_miliseconds} ms from the config file')
 
 states = statemachine.Statemachine(PYRO_FIRE_DELAY_MS = delay_pyro_miliseconds)
 
+# i2c = busio.I2C(board.SCL, board.SDA)
 i2c = board.I2C()
 
-import adafruit_adxl34x
-accelerometer = adafruit_adxl34x.ADXL343(i2c)
-# accelerometer.acceleration
+import adafruit_mpu6050
+accelerometer = adafruit_mpu6050.MPU6050(i2c)
+
 
 import adafruit_bmp280
-bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, 0x76)
 bmp280.sea_level_pressure = 1013.25
-# bmp280.pressure
-# bmp280.altitude
 
-with open("data.txt", "w") as fp:
-   while True:
-      buzzer.buzzer_tick()
-      states.tick()
+alt_counter = 0
+altmax = bmp280.altitude
 
-      fp.write(f"{accelerometer.acceleration} \n")
-      fp.write(f"Pressure: {bmp280.pressure}, Altitude: {bmp280.altitude} \n")
-      # print(accelerometer.acceleration)
-      # print(bmp280.pressure)
-      # print(bmp280.altitude)
+writedata = False
+
+while True:
+    buzzer.buzzer_tick()
+    states.tick()
+    altitude = bmp280.altitude
+    pressure = bmp280.pressure
+    acceleration = accelerometer.acceleration
+    # print(f"Acceleration: {acceleration} \n")
+    # print(f"Pressure: {pressure}, Altitude: {altitude} \n")
 
 
-# external.PYRO_DETONATE()
+    # print(time.time())
+
+    if(states.state == statemachine.States.LAUNCHED_MODE):
+        writedata = True
+
+        if altitude <= altmax:
+            alt_counter += 1
+        else:
+            alt_counter = 0
+        altmax = altitude
+
+        if alt_counter == 3:
+            external.PYRO_DETONATE()
+       
+            # print(accelerometer.acceleration)
+            # print(bmp280.pressure)
+            # print(bmp280.altitude)
+        time.sleep(1)
+
+    if writedata:
+        with open("data.txt", "w") as fp:
+            fp.write(f"Acceleration: {acceleration} \n")
+            fp.write(f"Pressure: {pressure}, Altitude: {altitude} \n")
+            # fp.write(str(time.time_ns()))
